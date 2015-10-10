5d5bc4e140dd4c5f6458d8d188f1a0d4343d2a95
linotelera <linotelera@gmail.com>
2015-10-10 20:34:34 +0200

    new file:   .project 	new file:   .pydevproject 	new file:   allVMs.py

---
+++ allVMs.py
@@ -0,0 +1,84 @@
+import argparse
+import atexit
+import urllib3.contrib.pyopenssl
+
+from pyVmomi import vim
+from pyVmomi import vmodl
+from pyVim import connect
+
+
+
+
+def get_args():
+    """
+   Supports the command-line arguments listed below.
+   """
+    parser = argparse.ArgumentParser(
+        description='Process args for retrieving all the Virtual Machines')
+
+    parser.add_argument('-s', '--host',
+                        required=True, action='store',
+                        help='Remote host to connect to')
+
+    parser.add_argument('-o', '--port',
+                        type=int, default=443,
+                        action='store', help='Port to connect on')
+
+    parser.add_argument('-u', '--user', required=True,
+                        action='store',
+                        help='User name to use when connecting to host')
+
+    parser.add_argument('-p', '--password',
+                        required=True, action='store',
+                        help='Password to use when connecting to host')
+
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """
+   Simple command-line program for listing all VM in a vcenter
+   """
+    import requests
+    requests.packages.urllib3.disable_warnings()
+    urllib3.contrib.pyopenssl.inject_into_urllib3()
+    
+    args = get_args()
+
+    try:
+        service_instance = connect.SmartConnect(host=args.host,
+                                                user=args.user,
+                                                pwd=args.password,
+                                                port=int(args.port))
+        if not service_instance:
+            print("Could not connect to the specified host using "
+                  "specified username and password")
+            return -1
+
+        atexit.register(connect.Disconnect, service_instance)
+
+        content = service_instance.RetrieveContent()
+        # Search for all Datastore Clusters aka StoragePod
+        
+        obj_view = content.viewManager.CreateContainerView(content.rootFolder,
+                                                           [vim.VirtualMachine],
+                                                           True)
+        vm_list = obj_view.view    
+        obj_view.Destroy()
+        
+        
+        for singlevm in vm_list:
+            print "VM: %s" % singlevm.name
+            
+
+    except vmodl.MethodFault as error:
+        print "Caught vmodl fault : " + error.msg
+        return -1
+
+    return 0
+
+# Start program
+if __name__ == "__main__":
+    main()
